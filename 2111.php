02bdda015eedae32f7f10639d6c492fec3a64132
Rodisnei <146873333+Rodisnei@users.noreply.github.com>
2023-11-21 16:58:03 -0300

    Add files via upload

---
+++ 2111.php
@@ -0,0 +1,71 @@
+</php
+
+$nome= "jefry";
+
+var_dump($nome);
+
+$largura= 30;
+$altura= 5;
+
+$area= $largura + $altura;
+
+2!= 2;
+2!= '2';
+
+int $idade=25;
+string $idade= '25';
+
+$idade != $indade;
+
+true && true;
+true && false:
+false && true;
+false && false;
+
+true and false;
+true and false;
+false and true;
+false and false;
+
+true || true;
+true || false;
+false || true;
+false || false;
+
+true or true;
+true or false;
+false or true;
+false or false;
+
+//operador not (!)
+$teste = true;
+
+!$teste; //false
+
+//--return true se os dois operandos forem iguais.
+//ja o ooperando === retorna true se os 2 operandos forem identicos
+
+1== '1'; //true
+1=== '1';//false
+
+1>2 || 2>1;
+1 !== 2 && 2>2;
+
+$idade = 20;
+
+$idade++; //A idade agora é 21.
+
+$idade--; //agora a idade é 20.
+
+$nome= 'Rodisnei';
+starlen ($nome); //7
+
+str_replace('snei', 'or', $nome) //fluor
+
+//trim (): remove o espaço em branco no inicio e no final de uma string.
+//strtoupper(): transforma uma string em maiúscula.
+//strtolower(): transforma uma string em minúscula.
+//Ucfirst(): transforma o primeiro caractere em minuscula.
+//implode(): junta os elementos de uma array em uma string.
+
+?>
